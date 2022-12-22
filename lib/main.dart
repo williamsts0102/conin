00f5ab05c582efa9fc4b224d52518bf66bf589d5
williamsts0102 <i201913010@cibertec.edu.pe>
2022-12-22 12:31:39 -0500

    agregando validaciones al login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:conin/routes/route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:conin/providers/provider_login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (_) => ProviderLogin(),
+      ),
+    ], child: const MyApp()),
+  ); // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/login_pages/login_page.dart
+++ lib/pages/login_pages/login_page.dart
@@ -2,6 +2,8 @@ import 'package:conin/routes/route.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:conin/providers/provider_login.dart';
 
 class LoginPageConin extends StatefulWidget {
   const LoginPageConin({super.key});
@@ -24,7 +26,7 @@ class _LoginPageConinState extends State<LoginPageConin> {
         child: Center(
           child: Container(
             height: MediaQuery.of(context).size.height * 1,
-            padding: const EdgeInsets.only(bottom: 10, top: 30),
+            padding: const EdgeInsets.only(bottom: 0, top: 10),
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                   begin: Alignment.topCenter,
@@ -43,7 +45,7 @@ class _LoginPageConinState extends State<LoginPageConin> {
                 children: [
                   SizedBox(
                     width: 300,
-                    height: 220,
+                    height: 205,
                     child: Lottie.asset('assets/lottie/login_conin.json'),
                     // child: Image.network(
                     //     'https://cdn.pixabay.com/photo/2022/12/12/12/58/dog-7651002_960_720.jpg'),
@@ -51,7 +53,9 @@ class _LoginPageConinState extends State<LoginPageConin> {
                   const SizedBox(
                     height: 0,
                   ),
-                  const _LoginForm(),
+                  ChangeNotifierProvider(
+                      create: (context) => ProviderLogin(),
+                      child: const _LoginForm()),
                   const SizedBox(
                     height: 50,
                   ),
@@ -97,19 +101,21 @@ class _LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<_LoginForm> {
-  bool _isPass = true;
+  bool _ispassowrd = true;
 
   void _viewPassword() {
     setState(() {
-      _isPass = !_isPass;
+      _ispassowrd = !_ispassowrd;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final loginProvider = Provider.of<ProviderLogin>(context);
+
     return SizedBox(
       child: Form(
-        // key: prov_login.formKey,
+        key: loginProvider.formkey,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
@@ -122,8 +128,15 @@ class _LoginFormState extends State<_LoginForm> {
                   hintText: 'Email',
                   prefixIcon: const Icon(Icons.email, color: Colors.black),
                 ),
-                onChanged: (value) {},
-                validator: (value) {},
+                onChanged: (value) => loginProvider.email = value,
+                validator: (value) {
+                  String caracteres =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = RegExp(caracteres);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'No es un correo valido';
+                },
               ),
               const SizedBox(
                 height: 15,
@@ -131,7 +144,7 @@ class _LoginFormState extends State<_LoginForm> {
               TextFormField(
                 style: const TextStyle(color: Colors.black),
                 autocorrect: false,
-                obscureText: _isPass,
+                obscureText: _ispassowrd,
                 keyboardType: TextInputType.text,
                 decoration: _builDecoration(
                   hintText: 'Contraseña',
@@ -140,14 +153,18 @@ class _LoginFormState extends State<_LoginForm> {
                     color: Colors.black,
                   ),
                   suffixIcon: InkWell(
+                    onTap: _viewPassword,
                     child: Icon(
-                      _isPass ? Icons.visibility : Icons.visibility_off,
+                      _ispassowrd ? Icons.visibility_off : Icons.visibility,
                     ),
-                    onTap: () {},
                   ),
                 ),
-                onChanged: (value) {},
-                validator: ((value) {}),
+                onChanged: (value) => loginProvider.password = value,
+                validator: (value) {
+                  return (value != null && value.length >= 8)
+                      ? null
+                      : 'La contraseña debe tener al menos 8 caracteres';
+                },
               ),
               const SizedBox(
                 height: 15,
@@ -162,15 +179,33 @@ class _LoginFormState extends State<_LoginForm> {
                   ),
                   disabledColor: Colors.purple,
                   elevation: 1,
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, MyRoutes.rHome);
-                  },
-                  child: const Text(
-                    'INGRESAR',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
+                  onPressed: loginProvider.isLoading
+                      ? null
+                      : () async {
+                          FocusScope.of(context).unfocus();
+                          if (!loginProvider.isValidForm()) return;
+
+                          loginProvider.isLoading = true;
+                          await Future.delayed(
+                            const Duration(seconds: 2),
+                          );
+                          loginProvider.isLoading = false;
+                          // ignore: use_build_context_synchronously
+                          Navigator.pushReplacementNamed(
+                              context, MyRoutes.rHome);
+                        },
+                  child: (loginProvider.isLoading)
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : const Text('INGRESAR',
+                          style: TextStyle(color: Colors.white)),
+                  // const Text(
+                  //   'INGRESAR',
+                  //   style: TextStyle(
+                  //     color: Colors.white,
+                  //   ),
+                  // ),
                 ),
               )
             ],
@@ -187,15 +222,15 @@ InputDecoration _builDecoration({
   final Widget? suffixIcon,
 }) {
   return InputDecoration(
-      errorStyle: const TextStyle(fontSize: 15, color: Colors.white60),
+      errorStyle: const TextStyle(fontSize: 15, color: Colors.red),
       enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 2, color: Colors.lightBlue),
+          borderSide: const BorderSide(width: 2, color: Colors.lightBlue),
           borderRadius: BorderRadius.circular(15)),
       focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 2, color: Colors.blueAccent),
+          borderSide: const BorderSide(width: 2, color: Colors.blueAccent),
           borderRadius: BorderRadius.circular(15)),
       border: OutlineInputBorder(
-        borderSide: BorderSide(width: 2, color: Colors.blueAccent),
+        borderSide: const BorderSide(width: 2, color: Colors.blueAccent),
         borderRadius: BorderRadius.circular(15),
       ),
       filled: true,
@@ -204,5 +239,5 @@ InputDecoration _builDecoration({
       hintStyle: const TextStyle(color: Colors.grey),
       prefixIcon: prefixIcon,
       suffixIcon: suffixIcon,
-      contentPadding: EdgeInsets.all(18));
+      contentPadding: const EdgeInsets.all(18));
 }
